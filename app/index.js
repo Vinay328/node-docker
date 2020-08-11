99f7ee9100496f92375429abbb6b0dec6fad2315
vinaykoti <vinay.koti@colortokens.com>
2020-08-11 17:56:36 +0530

    Added body-parser

--- app/index.js
+++ app/index.js
@@ -1,12 +1,53 @@
 const express = require('express');
+const fs = require('fs');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 // Constants
 const PORT = 8080;
 
 // App
 const app = express();
+const EXPRESS_LOG_FILE = process.env.EXPRESS_LOG_FILE || './access.log';
+
+const accessLogStream = fs.createWriteStream(`${EXPRESS_LOG_FILE}`, { flags: 'a' });
+
+app.use(logger('combined', { stream: accessLogStream }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(compression());
+
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.post('/', (req,res) => {
+    console.log("POST REQEUST CALLED");
+    try {
+        console.log(JSON.stringify(req.body));
+        console.log(JSON.stringify(req));
+
+    } catch (e) {
+        console.log(e);
+    }
+    res.status(201).json(req.body);
+});
+
+app.post('/validate', (req,res) => {
+    console.log("VALIDATE REQUEST CALLED");
+    try {
+        console.log(JSON.stringify(req.body));
+        console.log(JSON.stringify(req));
+
+    } catch (e) {
+        console.log(e);
+    }
+    res.status(201).json(req.body);
 });
 
 app.listen(PORT);
